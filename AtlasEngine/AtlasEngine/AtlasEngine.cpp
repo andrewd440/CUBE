aadbc18be999750fdb5525b7bc1c46b744740cfe
Andrew Lowry <alowry6@uncc.edu>
2015-01-07 17:35:58 -0500

    Updated SSEMath for column major matrices.

--- AtlasEngine/AtlasEngine/AtlasEngine.cpp
+++ AtlasEngine/AtlasEngine/AtlasEngine.cpp
@@ -22,34 +22,37 @@ int main()
 	clock_t t1, t2;
 	t1 = clock();
 
-	FMatrix4 Mat1{ 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16 };
-	//Mat1.Rotate(Vector3f(5, 100, 10));
+	FMatrix4 Mat1;
+	Mat1.Rotate(Vector3f(5, 100, 10));
 
-	FMatrix4 Mat2{ 1, 2.5f, 3, 4.75f, 5, 6, 7, 8, 9, 10, 11.22f, 12, 13, 14, 15, 16 };
-	//Mat2.SetOrigin(Vector3f(5, 0, 1));
-	//Mat2.Rotate(Vector3f(50, 5, 10));
+	FMatrix4 Mat2;
+	Mat2.SetOrigin(Vector3f(5, 0, 1));
+	Mat2.Rotate(Vector3f(50, 5, 10));
+	
+	Vector4f Vec(1, 2, 3, 4);
 
-	Vector4f Res = Mat1.TransformVector(Vector4f(1, 2, 3, 4));
-
-	uint8_t Char = 0x6F;
+	//uint8_t Char = 0x6F;
 
 	FMatrix4 Res1;
 
-	Res1 = Mat1 * Mat2;
+	//Res1 = Mat1 * Mat2;
 
-	std::cout << FString::HashCRC32("This") << std::endl;
-	std::cout << FString::HashCRC32("Tis") << std::endl;
-	std::cout << FString::HashCRC32("GoAndSees") << std::endl;
-	std::cout << FString::HashCRC32("Beast") << std::endl;
+	//std::cout << FString::HashCRC32("This") << std::endl;
+	//std::cout << FString::HashCRC32("Tis") << std::endl;
+	//std::cout << FString::HashCRC32("GoAndSees") << std::endl;
+	//std::cout << FString::HashCRC32("Beast") << std::endl;
+	Vector4f Res;
+	for (int i = 0; i < 1000000; i++)
+	{
+		//Res = Mat1.TransformVector(Vec);
+		//SSEMath::MultVectorMatrix(Mat1, Vec, Res);
+		//Mat1.Rotate(Vector3f(i, 0, 0));
+		//Res1 = Mat2 * Mat1;
+		SSEMath::MultMatrixMatrix(Mat2, Mat1, Res1);
+		Mat2.Rotate(Vector3f(i, 0, 0));
+	}
 
-	//for (int i = 0; i < 1000000; i++)
-	//{
-	//	Res1 = Mat2 * Mat1;
-	//	//SSEMath::MultMatrixMatrix(Mat2, Mat1, Res1);
-	//	Mat2.Rotate(Vector3f(i, 0, 0));
-	//}
-
-	std::cout << Mat2.M[0][0];
+	std::cout << Res[0] << std::endl;
 	t2 = clock();
 	std::cout << ((float)t2 - (float)t1) / CLOCKS_PER_SEC << std::endl;
 
--- AtlasEngine/AtlasEngine/Include/Math/SSEMath.h
+++ AtlasEngine/AtlasEngine/Include/Math/SSEMath.h
@@ -24,7 +24,8 @@
 namespace SSEMath
 {
 	/**
-	* Matrix - Vector multiplication with SSE SIMD instructions. 
+	* Matrix - Vector multiplication with SSE SIMD instructions.
+	* Matrix should be column-vector and stored as column-major.
 	* The FMatrix4 and Vector4f must be 16 byte aligned.
 	* @param Matrix to multiply
 	* @param Vector to multiply
@@ -33,23 +34,17 @@ namespace SSEMath
 	__forceinline void MultVectorMatrix(const FMatrix4& Matrix, const Vector4f& Vector, Vector4f& ResultOut)
 	{	
 		const __m128 V = _mm_load_ps(&Vector.x);
-		const __m128 MRows[4] = {
+		const __m128 MCols[4] = {
 			_mm_load_ps(Matrix.M[0]),
 			_mm_load_ps(Matrix.M[1]),
 			_mm_load_ps(Matrix.M[2]),
 			_mm_load_ps(Matrix.M[3])
 		};
 
-		// Multiply each row with the vector
-		const __m128 V0 = _mm_mul_ps(MRows[0], V);
-		const __m128 V1 = _mm_mul_ps(MRows[1], V);
-		const __m128 V2 = _mm_mul_ps(MRows[2], V);
-		const __m128 V3 = _mm_mul_ps(MRows[3], V);
-
-		// Add the totals for each vector component
-		const __m128 Sum0 = _mm_hadd_ps(V0, V1);
-		const __m128 Sum1 = _mm_hadd_ps(V2, V3);
-		const __m128 Result = _mm_hadd_ps(Sum0, Sum1);
+		__m128 Result = _mm_mul_ps(MCols[0], _mm_replicate_x_ps(V));
+		Result = _mm_madd_ps(MCols[1], _mm_replicate_y_ps(V), Result);
+		Result = _mm_madd_ps(MCols[2], _mm_replicate_z_ps(V), Result);
+		Result = _mm_madd_ps(MCols[3], _mm_replicate_w_ps(V), Result);
 
 		// Store the result
 		_mm_store_ps(&ResultOut.x, Result);
@@ -61,29 +56,29 @@ namespace SSEMath
 		const __m128* M2 = (const __m128*)&Mat2;
 		__m128* Result = (__m128*)&ResultOut;
 
-		// Mat1[0] * Mat2
-		__m128 R0 = _mm_mul_ps(_mm_replicate_x_ps(M1[0]), M2[0]);
-		R0 = _mm_madd_ps(_mm_replicate_y_ps(M1[0]), M2[1], R0);
-		R0 = _mm_madd_ps(_mm_replicate_z_ps(M1[0]), M2[2], R0);
-		R0 = _mm_madd_ps(_mm_replicate_w_ps(M1[0]), M2[3], R0);
+		// Mat1 * Mat2[0]
+		__m128 R0 = _mm_mul_ps(M1[0], _mm_replicate_x_ps(M2[0]));
+		R0 = _mm_madd_ps(M1[1], _mm_replicate_y_ps(M2[0]), R0);
+		R0 = _mm_madd_ps(M1[2], _mm_replicate_z_ps(M2[0]), R0);
+		R0 = _mm_madd_ps(M1[3], _mm_replicate_w_ps(M2[0]), R0);
 
-		// Mat1[1] * Mat2
-		__m128 R1 = _mm_mul_ps(_mm_replicate_x_ps(M1[1]), M2[0]);
-		R1 = _mm_madd_ps(_mm_replicate_y_ps(M1[1]), M2[1], R1);
-		R1 = _mm_madd_ps(_mm_replicate_z_ps(M1[1]), M2[2], R1);
-		R1 = _mm_madd_ps(_mm_replicate_w_ps(M1[1]), M2[3], R1);
+		// Mat1 * Mat2[1]
+		__m128 R1 = _mm_mul_ps(M1[0], _mm_replicate_x_ps(M2[1]));
+		R1 = _mm_madd_ps(M1[1], _mm_replicate_y_ps(M2[1]), R1);
+		R1 = _mm_madd_ps(M1[2], _mm_replicate_z_ps(M2[1]), R1);
+		R1 = _mm_madd_ps(M1[3], _mm_replicate_w_ps(M2[1]), R1);
 
-		// Mat1[2] * Mat2
-		__m128 R2 = _mm_mul_ps(_mm_replicate_x_ps(M1[2]), M2[0]);
-		R2 = _mm_madd_ps(_mm_replicate_y_ps(M1[2]), M2[1], R2);
-		R2 = _mm_madd_ps(_mm_replicate_z_ps(M1[2]), M2[2], R2);
-		R2 = _mm_madd_ps(_mm_replicate_w_ps(M1[2]), M2[3], R2);
+		// Mat1 * Mat2[2]
+		__m128 R2 = _mm_mul_ps(M1[0], _mm_replicate_x_ps(M2[2]));
+		R2 = _mm_madd_ps(M1[1], _mm_replicate_y_ps(M2[2]), R2);
+		R2 = _mm_madd_ps(M1[2], _mm_replicate_z_ps(M2[2]), R2);
+		R2 = _mm_madd_ps(M1[3], _mm_replicate_w_ps(M2[2]), R2);
 
-		// Mat1[3] * Mat2
-		__m128 R3 = _mm_mul_ps(_mm_replicate_x_ps(M1[3]), M2[0]);
-		R3 = _mm_madd_ps(_mm_replicate_y_ps(M1[3]), M2[1], R3);
-		R3 = _mm_madd_ps(_mm_replicate_z_ps(M1[3]), M2[2], R3);
-		R3 = _mm_madd_ps(_mm_replicate_w_ps(M1[3]), M2[3], R3);
+		// Mat1 * Mat2[3]
+		__m128 R3 = _mm_mul_ps(M1[0], _mm_replicate_x_ps(M2[3]));
+		R3 = _mm_madd_ps(M1[1], _mm_replicate_y_ps(M2[3]), R3);
+		R3 = _mm_madd_ps(M1[2], _mm_replicate_z_ps(M2[3]), R3);
+		R3 = _mm_madd_ps(M1[3], _mm_replicate_w_ps(M2[3]), R3);
 
 		// Store the result
 		Result[0] = R0;
