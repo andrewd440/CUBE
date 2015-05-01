47d15064807e5c0465a77ba4bc83a114bba0cf7d
Andrew Lowry <alowry6@uncc.edu>
2015-05-01 18:20:58 -0400

    AltasEngine name deletion

--- AtlasEngine/AtlasEngine/Include/Atlas/Bitsize.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-#include <bitset>
-namespace Atlas
-{
-    /**
-    * Max size of Components used
-    */
-    const int BITSIZE = 32;
-}
--- AtlasEngine/AtlasEngine/Shaders/DepthRender.frag
+++
@@ -1,8 +0,0 @@
-#version 430 core
-
-layout (location = 0) out vec4 Color;
-
-void main()
-{
-	Color = vec4(1.0);
-}
